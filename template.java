6a5c7e18ab79964601c824e97a758ac3155f0a85
creativeshim <creativeshim@gmail.com>
2018-02-08 03:03:45 -0500

    final changes

---
+++ template.java
@@ -0,0 +1,7 @@
+public class template{ //can change class name(= file name)
+  public static void main(String[] args){
+    System.out.println("Hello World");
+    //compile : javac template.java
+    //run : java template(class name)
+  }
+}
