e2415c66f49a10082b129f1b8221ba430d3cfa90
2015513 <2015513@SUNGYOONSHIM.clt.net>
2018-02-20 10:27:43 +0900

    practice BJ

---
+++ algorithm/src/algorithm/inputoutput/no_1924.java
@@ -0,0 +1,16 @@
+package algorithm.inputoutput;
+
+import java.util.Scanner;
+
+public class no_1924 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(); //정점의 개수
+		int m = sc.nextInt(); //간선의 개수
+		
+		
+	}
+
+}
---
+++ algorithm/src/algorithm/inputoutput/no_2557.java
@@ -0,0 +1,10 @@
+package algorithm.inputoutput;
+
+public class no_2557 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
